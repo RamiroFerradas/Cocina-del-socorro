798d47b56efdff81c8bd6213554818e60b193f6e
Ramiro Ferradas <ramirof@shifta.la>
2024-10-27 11:58:43 -0300

    fix login timeout

--- app/services/auth/login.ts
+++ app/services/auth/login.ts
@@ -1,25 +1,37 @@
 "use server";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import nookies from "nookies";
-import { deleteUserCookie } from "@/app/services/cookies/delete";
 import api from "@/app/lib/axios";
 
 export const loginUser = async (username: string, password: string) => {
+  const maxRetries = 3;
+  const retryDelay = 2000; // Tiempo de espera entre reintentos en milisegundos
+
+  const params = new URLSearchParams({
+    username,
+    password,
+  });
+
+  const retryRequest = async (retries: number): Promise<any> => {
+    try {
+      const response = await api.post("auth/token", params);
+      return response;
+    } catch (error: any) {
+      if (
+        retries > 0 &&
+        error.message.includes("FUNCTION_INVOCATION_TIMEOUT")
+      ) {
+        console.warn(`Retrying login request... (${maxRetries - retries + 1})`);
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
+        return retryRequest(retries - 1);
+      } else {
+        throw error;
+      }
+    }
+  };
+
   try {
-    const params = new URLSearchParams({
-      username,
-      password,
-    });
-
-    const response = await api.post("auth/token", params);
-
+    const response = await retryRequest(maxRetries);
     const { access_token } = response.data;
-    nookies.set(null, "fromClient", "value", {
-      maxAge: 30 * 24 * 60 * 60,
-      path: "/",
-    });
-
     cookies().set({
       name: "access_token",
       value: access_token,
@@ -27,19 +39,13 @@ export const loginUser = async (username: string, password: string) => {
       path: "/",
       maxAge: 60 * 60 * 12,
     });
+
     return access_token;
   } catch (error: any) {
     throw new Error(
-      error.response.data.detail.includes("Could not validate user")
+      error.response?.data?.detail.includes("Could not validate user")
         ? "Usuario o contraseña incorrectos."
-        : error.response.data.detail
+        : error.response?.data?.detail || "Error desconocido."
     );
   }
-};
-
-export const logoutUser = async () => {
-  "use server";
-
-  await deleteUserCookie();
-  redirect("/login");
 };
